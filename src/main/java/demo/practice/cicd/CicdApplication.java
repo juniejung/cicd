9788f535fee78770bf8d5819c3ae1de978d24b67
juniejung <jooeunjung0429@gmail.com>
2023-10-23 18:34:00 +0900

    gitignore update

--- src/main/java/demo/practice/cicd/CicdApplication.java
+++ src/main/java/demo/practice/cicd/CicdApplication.java
@@ -2,9 +2,17 @@ package demo.practice.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdApplication {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "CI/CD practice";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CicdApplication.class, args);
